8d6392092d595eb067aad4f16cef8f5ed87cd4fb
RuiXuqi <90179819+RuiXuqi@users.noreply.github.com>
2025-07-31 21:52:20 +0800

    Further rename

--- src/main/java/noisethreader/NoiseThreader.java
+++ src/main/java/noisethreader/NoiseThreader.java
@@ -9,7 +9,7 @@ import org.apache.logging.log4j.Logger;
 public class NoiseThreader {
     public static final String MODID = "noisethreader";
     public static final String VERSION = "1.1.0";
-    public static final String NAME = "NoiseThreader";
+    public static final String NAME = "NoiseThreaderMeow";
     public static final Logger LOGGER = LogManager.getLogger();
 	
 	@Instance(MODID)
